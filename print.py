48e8e999825324045e6bcb19149382e74ace8657
kalaivani13 <kalaiseenan@gmail.com>
2020-04-30 14:40:35 +0530

    added print.py|added range.py

---
+++ print.py
@@ -0,0 +1,9 @@
+import logging
+
+# Creating an object
+logger = logging.getLogger()
+
+logger.info("Hello World")
+logger.debug("This ia a debug msg")
+logger.critical("This is a critical msg")
+logger.error("This is a error msg ")
--- range.py
+++ range.py
@@ -1,1 +1,7 @@
-print.py  range.py
+# from past.builtins import range
+#
+# mylist = range(5)
+# assert mylist == [0, 1, 2, 3, 4]
+
+for mylist in range(5):
+    print(mylist)
